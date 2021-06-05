a7b3f4f3fc153483a67cb7de0b549f0f229b91b8
Forgetfull <oksana14888@gmail.com>
2021-06-05 10:04:05 +0500

    creating fucn fulldate

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 import * as PIXI from 'pixi.js'
 import notes from './notes'
+import { Container } from '@pixi/display';
+
+let dateNow = new Date();
+console.log(dateNow.getDate())
+
+//СОЗДАТЬ ЦИКЛ КОТ БУДЕТ ПОКАЗЫТЬ Д7 ДНЕЙ ОТ СЕГОДНЯШНЕГО
+function fullDate(){
+
+}
 
 const app = new PIXI.Application({
   width: window.innerWidth,
@@ -9,6 +18,9 @@ const app = new PIXI.Application({
   resolution: window.devicePixelRatio || 1
 });
 
+let containerItems = new Container()
+let containerWeek = new Container()
+app.stage.addChild(containerItems,containerWeek)
 
 let listNotes = notes.map((item)=>{
   return item.header
@@ -19,7 +31,7 @@ listNotes.forEach((element,index) => {
   let item = new PIXI.Text(element,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
   item.x = 10
   item.y = index * 30
-  app.stage.addChild(item)
+  containerItems.addChild(item)
 })
 
 
--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   context: path.join(__dirname, 'src'),
   entry: ['./js/main.js'],
-  // mode: 'none', // none development production
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'game.min.[hash:8].js',
@@ -26,12 +25,6 @@ module.exports = {
     new ImageminPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
       disable: process.env.NODE_ENV !== 'production',
-
-      // optipng: {
-      //   optimizationLevel: 4
-      // },
-
-      //seems better on mac this way
       pngquant: {
         verbose:true,
         quality: '80-90',
